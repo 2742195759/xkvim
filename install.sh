7d32a2ccd7ee1f52f1837de7fb057ea7bfce8c1a
xiongkun <xiongkun03@baidu.com>
2021-08-15 16:54:57 +0000

    update and install git

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
+apt update
+apt install -y git
+git pull
 ln -sf /root/xkvim/vimrc ~/.vimrc
 mkdir -p /tmp
 mv ~/.vim/plugin/xiongkun /tmp/xiongkun.bak
 ln -sf /root/xkvim/xiongkun ~/.vim/plugin/xiongkun
-apt update
-apt install -y git
