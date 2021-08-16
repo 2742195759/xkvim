f2933bc85deb662d866d6dc4c035aaefaac85d32
xiongkun <xiongkun03@baidu.com>
2021-08-16 05:07:24 +0000

    add git prompt for bash

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ ln -sf /root/xkvim/after ~/.vim/after
 mkdir -p /tmp
 mv ~/.vim/plugin/xiongkun /tmp/xiongkun.bak
 ln -sf /root/xkvim/xiongkun ~/.vim/plugin/xiongkun
+rm -rf /root/.bashrc
+rm -rf /root/.scripts
+ln -sf /root/xkvim/bashrc /root/.bashrc
+ln -sf /root/xkvim/bash_scripts /root/.scripts
+source /root/.bashrc
