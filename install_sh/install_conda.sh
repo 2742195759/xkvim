23feb057ca0532d13b7584f555ab5d797005d76c
xiongkun <xiongkun03@baidu.com>
2022-07-16 17:49:55 +0000

    sss

--- install_sh/install_conda.sh
+++ install_sh/install_conda.sh
@@ -1,7 +1,6 @@
 wget http://10.255.125.22:8082/software/Miniconda3-latest-Linux-x86_64.sh
 rm -rf /root/miniconda3
-yes | apt install lzma-dev
-./Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh
 eval "$(/root/miniconda3/bin/conda shell.bash hook)"
 source ~/.bashrc
 conda create -n paddle python==3.7
--- xiongkun/plugin/main.vim
+++ xiongkun/plugin/main.vim
@@ -205,7 +205,7 @@ augroup PopupPreview
     "autocmd CursorHoldI * cal TryOpenPreview()
 augroup END
 
-let g:enable_clangd=1
+let g:enable_clangd=0
 if (match(getcwd(), "/home/data/") == 0 || match(getcwd(), "/home/ssd2/") == 0)&& g:enable_clangd
     echo "Enable Clangd Server"
     "start auto cmd
