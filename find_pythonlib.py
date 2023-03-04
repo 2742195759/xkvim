b9fd0efd42681b774b12c63fea016798907d4273
xiongkun <xiongkun03@baidu.com>
2023-03-04 16:15:38 +0000

    fix library finder

--- find_pythonlib.py
+++ find_pythonlib.py
@@ -1,8 +1,15 @@
 import glob
 import platform
+import glob
+import platform
 version = ".".join(platform.python_version().split('.')[0:2])
-files = glob.glob(f'/usr/lib/python{version}/config-{version}*')
+prefixs = ["/usr/lib",
+           "/usr/local/lib",
+           ]
+files = []
+for prefix in prefixs:
+    files.extend(glob.glob(f'{prefix}/python{version}/config-{version}*'))
 if len(files) == 0:
-    print("[Error]:" + "Not Found libpython3.8.")
-else: 
+    print("[Error]:" + "Not Found Python.")
+else:
     print(files[0] + '/')
