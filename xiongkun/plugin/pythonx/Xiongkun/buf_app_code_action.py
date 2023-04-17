9ac9b8d8d0cb6ab9b541c31e8f4bd3679a9c1ef8
xiongkun <xiongkun03@baidu.com>
2023-04-17 02:44:30 +0000

    add some other tools in <M-a> Code Actin

--- xiongkun/plugin/pythonx/Xiongkun/buf_app_code_action.py
+++ xiongkun/plugin/pythonx/Xiongkun/buf_app_code_action.py
@@ -4,9 +4,12 @@ from .vim_utils import SetVimRegister
 import vim
 
 code_action_dict = {
+    "file finder       |文件模糊查找|": "FF",
+    "universe find     | 关键字查找 |": "call UniverseSearch()",
+    "google            |  谷歌查找  |": "Google",
     "baidu_fanyi       |  百度翻译  |": "Fanyi",
     "yiyan             |百度文心一言|": "YiyanTrigger", 
-    "preview window    |  QuickPeek |": "QuickPeek"
+    "preview window    |  QuickPeek |": "QuickPeek",
 }
 
 @vim_register(command="CodeAction", keymap="<m-a>")
