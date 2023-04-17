976b8d9f2712186ed70f63327e07be89f93410c7
xiongkun <xiongkun>
2023-04-17 09:44:03 +0800

    补充了5个CodeAction，而且他们可以互相调用，互相获取信息。

--- xiongkun/plugin/pythonx/Xiongkun/buf_app_code_action.py
+++ xiongkun/plugin/pythonx/Xiongkun/buf_app_code_action.py
@@ -4,9 +4,11 @@ from .vim_utils import SetVimRegister
 import vim
 
 code_action_dict = {
+    "file finder       |  文件查找  |": "FF", 
+    "universe search   | 关键字查找 |": "call UniverseSearch()", 
     "baidu_fanyi       |  百度翻译  |": "Fanyi",
     "yiyan             |百度文心一言|": "YiyanTrigger", 
-    "preview window    |  QuickPeek |": "QuickPeek"
+    "preview window    |  QuickPeek |": "QuickPeek",
 }
 
 @vim_register(command="CodeAction", keymap="<m-a>")
