9a38cc4e12ad8fff2ec6f492b1451b7843e304c3
xiongkun <xiongkun03@baidu.com>
2023-04-22 05:23:16 +0000

    refine yiyan.py

--- xiongkun/plugin/pythonx/Xiongkun/buf_app_code_action.py
+++ xiongkun/plugin/pythonx/Xiongkun/buf_app_code_action.py
@@ -4,14 +4,16 @@ from .vim_utils import SetVimRegister
 import vim
 
 code_action_dict = {
-    "file finder       |文件模糊查找  |": "FF",
-    "universe find     | 关键字查找   |": "call UniverseSearch()",
-    "google            |  谷歌查找    |": "GoogleUI",
-    "paddle doc        | Paddle文档   |": "PdocUI",
-    "baidu_fanyi       |  百度翻译    |": "Fanyi",
-    "yiyan             |百度文心一言  |": "YiyanTrigger", 
-    "preview window    |  QuickPeek   |": "QuickPeek",
-    "create tmp file   |创建新临时文件|": "tabe /tmp/tmp.py",
+    "file finder       |文件模糊查找    |": "FF",
+    "universe find     | 关键字查找     |": "call UniverseSearch()",
+    "google            |  谷歌查找      |": "GoogleUI",
+    "paddle doc        | Paddle文档     |": "PdocUI",
+    "baidu_fanyi       |  百度翻译      |": "Fanyi",
+    "yiyan             |百度文心一言    |": "YiyanTrigger", 
+    "yiyan login       |百度文心一言登录|": "YiyanLogin",
+    "yiyan code        |文心一言代码生成|": "YiyanCode",
+    "preview window    |  QuickPeek     |": "QuickPeek",
+    "create tmp file   |创建新临时文件  |": "tabe /tmp/tmp.py",
 }
 
 @vim_register(command="CodeAction", keymap="<m-a>")
@@ -23,6 +25,20 @@ def CodeAction(args):
     code_action = CommandList("[ CodeAction ]", keys, vals)
     code_action.create()
     code_action.show()
+
+vim.command(""" 
+inoremap <silent> <m-a> <cmd>CodeAction<cr>
+""")
+
+@vim_register(command="YiyanLogin")
+def YiyanDebug(args):
+    vim.command("tab terminal")
+    command = "/root/.local/share/pyppeteer/local-chromium/1000260/chrome-linux/chrome --disable-background-networking --disable-background-timer-throttling --disable-breakpad --disable-browser-side-navigation --disable-client-side-phishing-detection --disable-default-apps --disable-dev-shm-usage --disable-extensions --disable-features=site-per-process --disable-hang-monitor --disable-popup-blocking --disable-prompt-on-repost --disable-sync --disable-translate --metrics-recording-only --no-first-run --safebrowsing-disable-auto-update --password-store=basic --use-mock-keychain --remote-debugging-port=22 --remote-debugging-address=0.0.0.0 --user-data-dir=/root/xkvim/chrome-web/ --headless --hide-scrollbars --mute-audio about:blank --no-sandbox "
+    bufnr = int(vim.eval("bufnr()"))
+    send_keys(bufnr, command + '\n')
+
+def send_keys(bufnr, keys):
+    vim.eval(f"term_sendkeys({bufnr}, \"{keys}\")")
 
 vim.command(""" 
 inoremap <silent> <m-a> <cmd>CodeAction<cr>
