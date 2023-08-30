e168410ef6423827639e4ab9e5517d0451fd7d93
xiongkun <80377414@qq.com>
2023-08-30 15:19:46 +0800

    sss

--- xiongkun/plugin/pythonx/Xiongkun/rpc_server/servers/bash_server.py
+++ xiongkun/plugin/pythonx/Xiongkun/rpc_server/servers/bash_server.py
@@ -50,6 +50,8 @@ def bash_server(socket):
                 handle.wfile.write(bytes)
 
     # exit bash or killed.
+    rfile.close()
+    wfile.close()
     socket.close()
     print ("[Bash] exit bash server.")
     print("=== socket closed ===")
